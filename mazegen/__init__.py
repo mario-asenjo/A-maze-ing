dfc67a692cdb8f2e185ebaf9e067f06b6c1c2af4
Mario Asenjo Pérez <mario.asenjo@outlook.es>
2026-01-23 17:45:36 +0100

    feat(core): Finished maze.py and Maze data structure and helper functions, added tests for this implementation

---
+++ mazegen/__init__.py
@@ -0,0 +1,53 @@
+"""
+mazegen package public API.
+
+This allows imports from mazegen directly.
+"""
+
+
+from .errors import (
+    MazeConfigError,
+    MazeError,
+    MazeGenerationError,
+    MazeUnsolvableError
+)
+
+from .maze import (
+    ALL_WALLS,
+    N,
+    E,
+    S,
+    W,
+    Coord,
+    Direction,
+    Maze,
+    assert_neighbor_wall_consistency,
+    ensure_outer_borders_closed,
+    has_wall,
+    in_bounds,
+    neighbor_of,
+    set_wall,
+    set_wall_between
+)
+
+__all__ = [
+    "ALL_WALLS",
+    "N",
+    "E",
+    "S",
+    "W",
+    "Coord",
+    "Direction",
+    "Maze",
+    "MazeError",
+    "MazeConfigError",
+    "MazeGenerationError",
+    "MazeUnsolvableError",
+    "assert_neighbor_wall_consistency",
+    "ensure_outer_borders_closed",
+    "has_wall",
+    "in_bounds",
+    "neighbor_of",
+    "set_wall",
+    "set_wall_between"
+]
